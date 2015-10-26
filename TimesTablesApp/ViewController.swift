e6ce34b822b3fa4af74572c21ee5d315f41932dc
Mary Grace Lucas <graceylucas@hotmail.com>
2015-10-26 13:16:00 -0400

    Initial Commit

---
+++ TimesTablesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimesTablesApp
+//
+//  Created by Mary Grace Lucas on 10/26/15.
+//  Copyright © 2015 Mary Grace Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
